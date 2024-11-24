085a5d0fb9569ae7dec50d630566f45da03b8540
Christopher Geisel <cgeisel@lululemon.com>
2024-11-24 09:15:08 -0800

    finished tutorial, also added xp progress bar

--- render_functions.py
+++ render_functions.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import Tuple, TYPE_CHECKING
 
 import color
 
@@ -36,6 +36,32 @@ def render_bar(
     console.print(
         x=1, y=45, string=f"HP: {current_value}/{maximum_value}", fg=color.bar_text
     )
+
+def render_xp_bar(
+    console: Console, current_value: int, maximum_value: int, total_width: int
+) -> None:
+    bar_width = int(float(current_value) / maximum_value * total_width)
+
+    console.draw_rect(x=0, y=46, width=total_width, height=1, ch=1, bg=color.xp_bar_empty)
+
+    if bar_width > 0:
+        console.draw_rect(
+            x=0, y=46, width=bar_width, height=1, ch=1, bg=color.xp_bar_filled
+        )
+
+    console.print(
+        x=1, y=46, string=f"XP: {current_value}/{maximum_value}", fg=color.xp_bar_text
+    )
+
+def render_dungeon_level(
+    console: Console, dungeon_level: int, location: Tuple[int, int]
+) -> None:
+    """
+    Render the level the player is currently on, at the given location.
+    """
+    x, y = location
+
+    console.print(x=x, y=y, string=f"Dungeon level: {dungeon_level}")
 
 def render_names_at_mouse_location(
     console: Console, x: int, y: int, engine: Engine
